3279eeb785483083e28dcdd067de3f80521b6205
drneloms <drneloms@gmail.com>
2025-12-30 14:57:45 -0500

    Delete app/config/initializers/prometheus_exporter.rb

--- app/config/initializers/prometheus_exporter.rb
+++
@@ -1,6 +0,0 @@
-require "prometheus_exporter/middleware"
-require "prometheus_exporter/instrumentation"
-
-Rails.application.middleware.use PrometheusExporter::Middleware
-PrometheusExporter::Instrumentation::ActiveRecord.start
-PrometheusExporter::Instrumentation::Process.start(type: "web")
